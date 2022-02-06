0b1788dbc27ce1015fc45f17dbc85fee55fb8d34
PaulMalquin11 <87738066+PaulMalquin11@users.noreply.github.com>
2022-02-06 17:57:33 -0500

    Se agregó una lidea de codigo inservible

--- src/com/acme/PersonaRegistro.java
+++ src/com/acme/PersonaRegistro.java
@@ -3,6 +3,7 @@ package com.acme;
 public record PersonaRegistro(String nombre, String apellido) {
     @Override
     public String toString() {
+        System.out.println("Esta isntruccion es inservible");
         return "%s %s".formatted(nombre, apellido);
     }
 }
