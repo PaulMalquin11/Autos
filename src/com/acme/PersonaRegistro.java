e126c55bb833cdbabfa32a8efccc2ebb76ad0504
PaulMalquin11 <87738066+PaulMalquin11@users.noreply.github.com>
2022-02-06 18:09:38 -0500

    Se quito la linea de cod inservible

--- src/com/acme/PersonaRegistro.java
+++ src/com/acme/PersonaRegistro.java
@@ -3,8 +3,6 @@ package com.acme;
 public record PersonaRegistro(String nombre, String apellido) {
     @Override
     public String toString() {
-        //ABC Comente la linea de cod que se puede eliminar
-       // System.out.println("Esta isntruccion es inservible");
         return "%s %s".formatted(nombre, apellido);
     }
 }
